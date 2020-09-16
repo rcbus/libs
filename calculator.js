bfd00c8fdc6a00c5137c2d5818ed89db34925233
rcbus <cleiton.rce@gmail.com>
2020-09-16 09:31:26 -0300

    update_cleiton_20200916_0931

--- calculator.js
+++ calculator.js
@@ -55,3 +55,60 @@ export function basic(data,variable){
 
     return data
 }
+
+export function complex(data,variable,calculation){
+    calculation.map(c => {
+        if(c.operator=='='){
+            var tempB = 0
+            if(strlen(c.value)){
+                tempB = formatNumber(c.value,10,0)
+            }else if(verifyVariable(variable[c.variableB])){
+                tempB = formatNumber(data[variable[c.variableB].description],variable[c.variableB].precision,variable[c.variableB].type)
+            }
+            if(verifyVariable(variable[c.variableA])){
+                data[variable[c.variableA].description] = formatNumber(tempB,variable[c.variableA].precision,variable[c.variableA].type)
+            }
+        }else{
+            var tempA = formatNumber(data[variable[c.variableA].description],variable[c.variableA].precision,variable[c.variableA].type)
+            var tempB = 0
+            var tempR = 0
+            if(strlen(c.value)){
+                tempB = formatNumber(c.value,10,0)
+            }else if(verifyVariable(variable[c.variableB])){
+                tempB = formatNumber(data[variable[c.variableB].description],variable[c.variableB].precision,variable[c.variableB].type)
+            }
+            if(c.operator=='/'){
+                if(tempB>0){
+                    tempR = (tempA / tempB)
+                }
+            }else if(c.operator=='*'){
+                tempR = (tempA * tempB)
+            }else if(c.operator=='+'){
+                tempR = (tempA + tempB)
+            }else if(c.operator=='-'){
+                tempR = (tempA - tempB)
+            }
+            if(verifyVariable(variable[c.variableR])){
+                data[variable[c.variableR].description] = formatNumber(tempR,variable[c.variableR].precision,variable[c.variableR].type)
+            }
+        }
+    })
+    return data
+}
+
+export function formatNumber(number,precision,type){
+    if(type == 0 && verifyVariable(number) && verifyVariable(precision)){
+        if(number!=null){
+            if(strlen(number)==0){
+                number = 0
+            }
+            return (parseFloat(number).toFixed(precision) * 1)
+        }else{
+            return 0
+        }
+    }else if(number!=null){
+        return number
+    }else{
+        return ''
+    }
+}
