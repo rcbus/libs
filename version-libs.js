73f8439f0f6d5bda05db4b9843007ed9d9a3a45c
rcbus <cleiton.rce@gmail.com>
2020-09-21 16:29:35 -0300

    update_cleiton_20200805_0138

--- version-libs.js
+++ version-libs.js
@@ -1,4 +1,4 @@
-const version = '1.2.5';
+const version = '1.2.6';
 
 export default function Version() {
   return (
@@ -11,6 +11,7 @@ export default function Version() {
 /*
 Histórico de versões
 
+1.2.6 - 21/09/2020
 1.2.5 - 18/09/2020
 1.2.4 - 17/09/2020
 1.2.3 - 17/09/2020
